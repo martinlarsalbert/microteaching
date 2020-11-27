54d3204cdd07a5a6689aff5cf4d47314a8a1d0b2
Martin Alexandersson <maa@sspa.se>
2020-11-27 08:05:03 +0100

    forgott this file

--- notebooks/animations.py
+++ notebooks/animations.py
@@ -126,3 +126,64 @@ class RollDecayAnimation(animation.TimedAnimation):
 
         for l in lines:
             l.set_data([], [])
+
+
+class ParametricRollAnimation(animation.TimedAnimation):
+    def __init__(self, ts, phis, Cs):
+        
+        fig,axes = plt.subplots(ncols=2)
+        fig.set_size_inches(12,8)
+        self.fig = fig
+        self.axes=axes
+        
+        self.t = ts
+        self.phis = phis
+        self.Cs = Cs
+        
+        ax = axes[0]
+        self.ship=add_ship(ax=ax)
+        
+        ax = axes[1]
+        
+        ax_C = ax.twinx()
+
+        ax.set_ylim(np.min(np.rad2deg(self.phis)),np.rad2deg(np.max(self.phis)))
+        ax.set_xlim(0,self.t[-1])
+        ax.set_xlabel('Time [s]')
+        #ax.set_ylabel('roll [deg]')
+        #ax.grid(True)
+            
+        self.phi_time = Line2D([], [], color='red', linewidth=4, label='roll')
+        self.C_time = Line2D([], [], color='green', linewidth=4, label='stability')
+
+        ax.add_line(self.phi_time)
+        ax_C.add_line(self.C_time)
+        
+        ax_C.set_ylim(np.min(self.Cs),np.max(self.Cs))
+        ax_C.set_xlim(0,self.t[-1])
+        ax_C.legend(loc='upper right')
+        ax.legend(loc='upper left')
+        
+        return animation.TimedAnimation.__init__(self, fig, interval=30, blit=True)
+
+    def _draw_frame(self, framedata):
+        i = framedata
+        
+        phi=self.phis[i]
+        times = np.arange(i, dtype=int)
+        self.phi_time.set_data(self.t[times],np.rad2deg(self.phis[times]))  
+        self.C_time.set_data(self.t[times],self.Cs[times])  
+        
+
+        rotate_ship(ship=self.ship, ax=self.axes[0], angle=phi)
+        
+        self._drawn_artists = [self.phi_time, self.C_time, self.ship]
+                
+    def new_frame_seq(self):
+        return iter(range(self.t.size))
+
+    def _init_draw(self):
+        lines = [self.phi_time]
+
+        for l in lines:
+            l.set_data([], [])
